e56ecc572d7da130edf20bb811e445a5f902dfd2
prajjwol0 <prajjwolpyakurel11@gmail.com>
2025-12-29 19:40:34 +0545

    ques number 6 (Unit 4)

--- unit4/ques6.php
+++ unit4/ques6.php
@@ -2,14 +2,34 @@
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <form action="" method="post">
-        
+        <input type="radio" name="language" value="PHP" id="php">
+        <label for="php">PHP</label><br>
+        <input type="radio" name="language" value="JavaScript" id="js">
+        <label for="js">JavaScript</label><br>
+        <input type="radio" name="language" value="Python" id="python">
+        <label for="python">Python</label><br><br>
+
+        <input type="submit" value="Submit">
     </form>
+    </form>
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (isset($_POST['language'])) {
+        echo "You selected: " . htmlspecialchars($_POST['language']);
+    } else {
+        echo "You did not select any language.";
+    }
+}
+?>
+
 </body>
 </html>
